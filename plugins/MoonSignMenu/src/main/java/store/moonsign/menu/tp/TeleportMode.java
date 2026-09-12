53858a4c2c6ac0c648461e7e143fa14e91523b33
MenkiPlugcore <cfirmansyah327@gmail.com>
2026-09-12 09:53:36 +0700

    chore: move MoonSignMenu to standalone CdrMemberBook repository

--- plugins/MoonSignMenu/src/main/java/store/moonsign/menu/tp/TeleportMode.java
+++
@@ -1,6 +0,0 @@
-package store.moonsign.menu.tp;
-
-public enum TeleportMode {
-    TO_TARGET,
-    TARGET_TO_REQUESTER
-}
--- plugins/MoonSignMenu/src/main/java/store/moonsign/menu/util/Colors.java
+++
@@ -1,9 +0,0 @@
-package store.moonsign.menu.util;
-
-public final class Colors {
-    private Colors() {}
-
-    public static String legacy(String value) {
-        return value == null ? "" : value.replace('&', '§');
-    }
-}
